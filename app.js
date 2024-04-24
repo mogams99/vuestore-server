901a4015c7c4b292894a384bc4f1d38198d5aab0
Mochammad Gamal <mogams1811@gmail.com>
2024-04-24 10:23:09 +0700

    chore: :construction: adding config to setup database mongo with mongoose

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const url = 'http://localhost';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const db = require('./app/models');
+db.mongoose
+    .connect(db.url)
+    .then(res => {
+        console.log('Database is connected.');
+    }).catch(err => {
+        console.log('Cannot connect to database.', err);
+        process.exit();
+    });
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to Vue Store Server'
@@ -13,5 +23,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`This app listening on ${url}:${port}`)
+  console.log(`This app listening on ${url}:${port}.`)
 });
